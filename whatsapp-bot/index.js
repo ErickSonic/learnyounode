fc7bee402c58ba46bef047ec6f5f84d9a9f1d078
ErickSonic <47461426+ErickSonic@users.noreply.github.com>
2022-03-16 01:35:50 -0600

    v1.3
    Major changes.

--- whatsapp-bot/index.js
+++ whatsapp-bot/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const contactRoutes = require('./routes/contact')
+const client = require('./bot/whats')
+require("dotenv").config();
 
 const app = express();
 const port = 6061;
+
+app.use(express.json());
+app.use('/api', contactRoutes)
 
 //API route
 app.get('/', (req, res) => {
@@ -10,6 +16,11 @@ app.get('/', (req, res) => {
 });
 
 //Connection to MongoDB Database
-mongoose.connect("mongodb+srv://ErickSonic:ERick280103@whatsapp.yjezr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("Connected to Atlas Database"))
+        .catch((error) => console.error(error));
+
+//Initialize Whatsapp Client
+client.initialize();
 
 app.listen(port, () => console.log('Running express app!'));
